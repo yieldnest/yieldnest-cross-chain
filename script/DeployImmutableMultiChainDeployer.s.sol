041152cf9e5a907360fb87d46fed50473693a3de
MrDeadCe11 <mister.deadce11@gmail.com>
2024-09-05 00:40:06 -0500

    working on json inputs

--- script/DeployImmutableMultiChainDeployer.s.sol
+++ script/DeployImmutableMultiChainDeployer.s.sol
@@ -3,15 +3,17 @@ pragma solidity ^0.8.13;
 
 import {Script, console} from "forge-std/Script.sol";
 import {ImmutableMultiChainDeployer} from "@factory/ImmutableMultiChainDeployer.sol";
-//forge script script/DeployImmutableMultiChainDeployer.s.sol:DeployImmutableMultiChainDeployer --rpc-url ${rpc}  --account ${deployerAccountName} --sender ${deployer} --broadcast --etherscan-api-key ${api} --verify
+//forge script script/DeployImmutableMultiChainDeployer.s.sol:DeployImmutableMultiChainDeployer --rpc-url ${rpc} --sig "run(string memory)" ${path} --account ${deployerAccountName} --sender ${deployer} --broadcast --etherscan-api-key ${api} --verify
 
 contract DeployImmutableMultiChainDeployer is Script {
+    address public multiChainDeployerAddress;
+
     function setUp() public {}
 
-    function run() public {
+    function run(string memory _salt) public {
         vm.broadcast();
-        address immutableMultiChainDeployer = address(new ImmutableMultiChainDeployer{salt: "SALT"}());
+        multiChainDeployerAddress = address(new ImmutableMultiChainDeployer{salt: _salt}());
 
-        console.log("ImmutableMultiChainDeployer deployed at: ", immutableMultiChainDeployer);
+        console.log("ImmutableMultiChainDeployer deployed at: ", multiChainDeployerAddress);
     }
 }
