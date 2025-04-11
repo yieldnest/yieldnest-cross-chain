f30cb57221773766d6e3a211d314af3b58e62af0
MrDeadCe11 <mister.deadce11@gmail.com>
2025-04-11 16:37:32 +0530

    added verifications for enforced options, dvn and executor config

--- script/deploy/4_VerifyOFT.s.sol
+++ script/deploy/4_VerifyOFT.s.sol
@@ -4,21 +4,34 @@ pragma solidity ^0.8.24;
 
 import {BaseScript, ChainDeployment, PeerConfig, ReceiveLibConfig, SendLibConfig} from "../BaseScript.s.sol";
 import {BatchScript} from "../BatchScript.s.sol";
+
+import {L2YnERC20Upgradeable} from "@/L2YnERC20Upgradeable.sol";
+import {L2YnOFTAdapterUpgradeable} from "@/L2YnOFTAdapterUpgradeable.sol";
+import {ExecutorConfig} from "@layerzerolabs/lz-evm-messagelib-v2/contracts/SendLibBase.sol";
+
+import {IOAppCore} from "@layerzerolabs/lz-evm-oapp-v2/contracts/oapp/interfaces/IOAppCore.sol";
+import {RateLimiter} from "@layerzerolabs/lz-evm-oapp-v2/contracts/oapp/utils/RateLimiter.sol";
 import {
     ILayerZeroEndpointV2,
     IMessageLibManager
 } from "@layerzerolabs/lz-evm-protocol-v2/contracts/interfaces/ILayerZeroEndpointV2.sol";
 
-import {L2YnERC20Upgradeable} from "@/L2YnERC20Upgradeable.sol";
-import {L2YnOFTAdapterUpgradeable} from "@/L2YnOFTAdapterUpgradeable.sol";
+import {OAppOptionsType3Upgradeable} from
+    "@layerzerolabs/oapp-evm-upgradeable/contracts/oapp/libs/OAppOptionsType3Upgradeable.sol";
+import {EnforcedOptionParam} from "@layerzerolabs/oapp-evm/contracts/oapp/interfaces/IOAppOptionsType3.sol";
 
-import {IOAppCore} from "@layerzerolabs/lz-evm-oapp-v2/contracts/oapp/interfaces/IOAppCore.sol";
-import {RateLimiter} from "@layerzerolabs/lz-evm-oapp-v2/contracts/oapp/utils/RateLimiter.sol";
+import {IOAppOptionsType3} from "@layerzerolabs/oapp-evm/contracts/oapp/interfaces/IOAppOptionsType3.sol";
 import {TimelockController} from "@openzeppelin/contracts/governance/TimelockController.sol";
 import {ProxyAdmin} from "@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol";
 import {console} from "forge-std/console.sol";
 
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+import {SetConfigParam} from "@layerzerolabs/lz-evm-protocol-v2/contracts/interfaces/IMessageLibManager.sol";
+
+import {UlnConfig} from "@layerzerolabs/lz-evm-messagelib-v2/contracts/uln/UlnBase.sol";
+
+import {ExecutorConfig} from "@layerzerolabs/lz-evm-messagelib-v2/contracts/SendLibBase.sol";
 
 // forge script VerifyOFT --rpc-url ${rpc} \
 // --sig "run(string calldata,string calldata)" ${input_path} ${deployment_path} \
@@ -29,6 +42,9 @@ contract VerifyOFT is BaseScript, BatchScript {
     PeerConfig[] public newPeers;
     SendLibConfig[] public newSendLibs;
     ReceiveLibConfig[] public newReceiveLibs;
+    bytes[] public newEnforcedOptions;
+    bytes[] public newDvns;
+    bytes[] public newExecutor;
 
     function run(
         string calldata _jsonPath,
@@ -147,33 +163,103 @@ contract VerifyOFT is BaseScript, BatchScript {
 
             ChainDeployment memory chainDeployment = findChainDeployment(chainId);
 
-            (,, uint256 limit, uint256 window) = RateLimiter(payable(currentDeployment.oftAdapter)).rateLimits(eid);
-            if (limit != baseInput.rateLimitConfig.limit || window != baseInput.rateLimitConfig.window) {
-                needsUpdate = true;
-                newRateLimitConfigs.push(
-                    RateLimiter.RateLimitConfig(
-                        eid, baseInput.rateLimitConfig.limit, baseInput.rateLimitConfig.window
-                    )
-                );
+            {
+                // verify rate limits
+                (,, uint256 limit, uint256 window) =
+                    RateLimiter(payable(currentDeployment.oftAdapter)).rateLimits(eid);
+                if (limit != baseInput.rateLimitConfig.limit || window != baseInput.rateLimitConfig.window) {
+                    needsUpdate = true;
+                    newRateLimitConfigs.push(
+                        RateLimiter.RateLimitConfig(
+                            eid, baseInput.rateLimitConfig.limit, baseInput.rateLimitConfig.window
+                        )
+                    );
+                }
             }
             if (chainId == block.chainid) {
                 continue;
             }
-            address adapter = chainDeployment.oftAdapter;
-            bytes32 adapterBytes32 = addressToBytes32(adapter);
-            if (IOAppCore(currentDeployment.oftAdapter).peers(eid) != adapterBytes32) {
-                needsUpdate = true;
-                newPeers.push(PeerConfig(eid, adapter));
-            }
-            if (lzEndpoint.getSendLibrary(currentDeployment.oftAdapter, eid) != getData(block.chainid).LZ_SEND_LIB)
             {
-                needsUpdate = true;
-                newSendLibs.push(SendLibConfig(eid, getData(block.chainid).LZ_SEND_LIB));
+                // verify peers
+                bytes32 adapterBytes32 = addressToBytes32(chainDeployment.oftAdapter);
+                if (IOAppCore(currentDeployment.oftAdapter).peers(eid) != adapterBytes32) {
+                    needsUpdate = true;
+                    newPeers.push(PeerConfig(eid, chainDeployment.oftAdapter));
+                }
             }
-            (address lib, bool isDefault) = lzEndpoint.getReceiveLibrary(adapter, eid);
-            if (lib != getData(block.chainid).LZ_RECEIVE_LIB && isDefault != false) {
-                needsUpdate = true;
-                newReceiveLibs.push(ReceiveLibConfig(eid, getData(block.chainid).LZ_RECEIVE_LIB));
+            {
+                // verify send library
+                if (
+                    lzEndpoint.getSendLibrary(currentDeployment.oftAdapter, eid)
+                        != getData(block.chainid).LZ_SEND_LIB
+                ) {
+                    needsUpdate = true;
+                    newSendLibs.push(SendLibConfig(eid, getData(block.chainid).LZ_SEND_LIB));
+                }
+            }
+            {
+                // verify receive library
+                (address lib, bool isDefault) = lzEndpoint.getReceiveLibrary(currentDeployment.oftAdapter, eid);
+                if (lib != getData(block.chainid).LZ_RECEIVE_LIB && isDefault != false) {
+                    needsUpdate = true;
+                    newReceiveLibs.push(ReceiveLibConfig(eid, getData(block.chainid).LZ_RECEIVE_LIB));
+                }
+            }
+            {
+                // verify enforced options
+                EnforcedOptionParam[] memory enforcedOptions = _getEnforcedOptions(chainId);
+                if (
+                    keccak256(OAppOptionsType3Upgradeable(currentDeployment.oftAdapter).enforcedOptions(eid, 1))
+                        != keccak256(enforcedOptions[0].options)
+                        || keccak256(OAppOptionsType3Upgradeable(currentDeployment.oftAdapter).enforcedOptions(eid, 2))
+                            != keccak256(enforcedOptions[1].options)
+                ) {
+                    needsUpdate = true;
+                    newEnforcedOptions.push(getConfigureEnforcedOptionsTX(chainId));
+                }
+            }
+
+            {
+                // verify dvns
+                bytes memory ulnConfig = abi.encode(_getUlnConfig());
+                if (
+                    keccak256(
+                        lzEndpoint.getConfig(
+                            currentDeployment.oftAdapter,
+                            getData(block.chainid).LZ_RECEIVE_LIB,
+                            eid,
+                            CONFIG_TYPE_ULN
+                        )
+                    ) != keccak256(ulnConfig)
+                        || keccak256(
+                            lzEndpoint.getConfig(
+                                currentDeployment.oftAdapter, getData(block.chainid).LZ_SEND_LIB, eid, CONFIG_TYPE_ULN
+                            )
+                        ) != keccak256(ulnConfig)
+                ) {
+                    needsUpdate = true;
+                    (bytes memory encodedSendTx, bytes memory encodedReceiveTx) = getConfigureDVNsTX(chainId);
+                    newDvns.push(encodedSendTx);
+                    newDvns.push(encodedReceiveTx);
+                }
+            }
+            {
+                // verify executor
+                ExecutorConfig memory executorConfig = _getExecutorConfig();
+                if (
+                    keccak256(
+                        lzEndpoint.getConfig(
+                            currentDeployment.oftAdapter,
+                            getData(block.chainid).LZ_SEND_LIB,
+                            eid,
+                            CONFIG_TYPE_EXECUTOR
+                        )
+                    ) != keccak256(abi.encode(executorConfig))
+                ) {
+                    needsUpdate = true;
+                    (bytes memory encodedSendTx) = getConfigureExecutorTX(chainIds[i]);
+                    newDvns.push(encodedSendTx);
+                }
             }
         }
 
@@ -295,7 +381,85 @@ contract VerifyOFT is BaseScript, BatchScript {
                 }
             }
 
+            if (newEnforcedOptions.length > 0) {
+                console.log("The following enforced options need to be set: ");
+                console.log("");
+                for (uint256 i = 0; i < newEnforcedOptions.length; i++) {
+                    console.log("Encoded Tx Data: ");
+                    console.logBytes(newEnforcedOptions[i]);
+                    addToBatch(address(lzEndpoint), newEnforcedOptions[i]);
+                    console.log("");
+                }
+            }
+
+            if (newDvns.length > 0) {
+                console.log("The following DVNs need to be set: ");
+                console.log("");
+                for (uint256 i = 0; i < newDvns.length; i++) {
+                    console.log("Encoded Tx Data: ");
+                    console.logBytes(newDvns[i]);
+                    addToBatch(address(lzEndpoint), newDvns[i]);
+                    console.log("");
+                }
+            }
+
+            if (newExecutor.length > 0) {
+                console.log("The following executor need to be set: ");
+                console.log("");
+                for (uint256 i = 0; i < newExecutor.length; i++) {
+                    console.log("Encoded Tx Data: ");
+                    console.logBytes(newExecutor[i]);
+                    addToBatch(address(lzEndpoint), newExecutor[i]);
+                    console.log("");
+                }
+            }
+
             displayBatch();
         }
+    }
+
+    function getConfigureEnforcedOptionsTX(uint256 dstChainId)
+        internal
+        view
+        returns (bytes memory encodedEnforcedOptions)
+    {
+        EnforcedOptionParam[] memory enforcedOptions = _getEnforcedOptions(dstChainId);
+
+        encodedEnforcedOptions =
+            abi.encodeWithSelector(IOAppOptionsType3.setEnforcedOptions.selector, enforcedOptions);
+    }
+
+    function getConfigureDVNsTX(uint256 dstChainId)
+        internal
+        view
+        returns (bytes memory encodedSendTx, bytes memory encodedReceiveTx)
+    {
+        Data storage data = getData(block.chainid);
+        uint32 dstEid = getEID(dstChainId);
+
+        UlnConfig memory ulnConfig = _getUlnConfig();
+
+        SetConfigParam[] memory params = new SetConfigParam[](1);
+        params[0] = SetConfigParam(dstEid, CONFIG_TYPE_ULN, abi.encode(ulnConfig));
+
+        encodedSendTx = abi.encodeWithSelector(
+            IMessageLibManager.setConfig.selector, currentDeployment.oftAdapter, data.LZ_SEND_LIB, params
+        );
+        encodedReceiveTx = abi.encodeWithSelector(
+            IMessageLibManager.setConfig.selector, currentDeployment.oftAdapter, data.LZ_RECEIVE_LIB, params
+        );
+    }
+
+    function getConfigureExecutorTX(uint256 dstChainId) internal view returns (bytes memory encodedExecutorTx) {
+        Data storage data = getData(block.chainid);
+        uint32 dstEid = getEID(dstChainId);
+        ExecutorConfig memory executorConfig = _getExecutorConfig();
+
+        SetConfigParam[] memory params = new SetConfigParam[](1);
+        params[0] = SetConfigParam(dstEid, CONFIG_TYPE_EXECUTOR, abi.encode(executorConfig));
+
+        encodedExecutorTx = abi.encodeWithSelector(
+            IMessageLibManager.setConfig.selector, currentDeployment.oftAdapter, data.LZ_SEND_LIB, params
+        );
     }
 }
