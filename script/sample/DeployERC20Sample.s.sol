04ee5a800fd1301bd7438fb770fb7cf2e7fb32e0
danoctavian <danoctavian91@gmail.com>
2025-11-04 17:48:20 +0200

    add sample token for testing

---
+++ script/sample/DeployERC20Sample.s.sol
@@ -0,0 +1,17 @@
+/* solhint-disable no-console, gas-custom-errors */
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {Script} from "forge-std/Script.sol";
+
+import {console} from "forge-std/console.sol";
+import {ERC20Sample} from "src/sample/ERC20Sample.sol";
+
+contract DeployERC20Sample is Script {
+    function run() public {
+        vm.startBroadcast();
+        ERC20Sample sampleContract = new ERC20Sample("ERC20Sample 1", "ERC20Sample-1");
+        console.log("SampleContract deployed at:", address(sampleContract));
+        vm.stopBroadcast();
+    }
+}
